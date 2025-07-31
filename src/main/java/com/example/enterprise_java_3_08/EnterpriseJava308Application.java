aaefdfb037253ddf3392fce1d8cb7bb4ae0f3fc6
salvatore marchese <smarchese985@yahoo.co.uk>
2025-07-31 14:47:45 +0200

    Initial SpringBoot project setup

---
+++ src/main/java/com/example/enterprise_java_3_08/EnterpriseJava308Application.java
@@ -0,0 +1,13 @@
+package com.example.enterprise_java_3_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseJava308Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseJava308Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/enterprise_java_3_08/EnterpriseJava308ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enterprise_java_3_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseJava308ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
